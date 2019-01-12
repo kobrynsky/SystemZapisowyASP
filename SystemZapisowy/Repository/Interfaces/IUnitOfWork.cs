91abc850aac231d5f116aaaf3cec004cf0eea4f2
Szymon Pituła <deesugfx@gmail.com>
2019-01-13 00:40:48 +0100

    Added UnitOfWork

---
+++ SystemZapisowy/Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SystemZapisowy.Repository.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICourseRepository Courses { get; }
+        int Complete();
+    }
+}
