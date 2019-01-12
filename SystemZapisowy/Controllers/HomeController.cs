bb808bb2ea60b4b5e6507a17c6ab20f37cede130
blc132 <pawelkob97@wp.pl>
2019-01-12 19:34:55 +0100

    added database files

--- SystemZapisowy/Controllers/HomeController.cs
+++ SystemZapisowy/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace SystemZapisowy.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
---
+++ SystemZapisowy/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemZapisowy.Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetOverview(Func<T, bool> predicate = null);
+        T GetDetail(Func<T, bool> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+    }
+}
+
