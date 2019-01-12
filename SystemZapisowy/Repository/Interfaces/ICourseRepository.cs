16220bc51ff3c520f806e4cb285dbaf8dafed624
Szymon Pituła <deesugfx@gmail.com>
2019-01-13 00:30:49 +0100

    Added example repository class

---
+++ SystemZapisowy/Repository/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SystemZapisowy.Repository.Interfaces
+{
+    public interface ICourseRepository : IRepository<Courses>
+    {
+        IEnumerable<Courses> GetCoursesWithGroups(int pageIndex, int pageSize);
+    }
+}
