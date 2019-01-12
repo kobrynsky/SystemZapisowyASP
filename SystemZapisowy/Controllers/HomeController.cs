20df850a4bbb4cdae295eaf3f744da6b3c218438
Szymon Pituła <deesugfx@gmail.com>
2019-01-13 00:57:29 +0100

    Added working with uow example to home controller

--- SystemZapisowy/Controllers/HomeController.cs
+++ SystemZapisowy/Controllers/HomeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SystemZapisowy.Repository;
+using SystemZapisowy.Repository.Interfaces;
 
 namespace SystemZapisowy.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        // for DI(?)
+        //public HomeController(IUnitOfWork unitOfWork)
+        //{
+        //    _unitOfWork = unitOfWork;
+        //}
+
+        public HomeController()
+        {
+            _unitOfWork = new UnitOfWork(new SystemZapisowyEntities());
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var courses = _unitOfWork.Courses.GetAll();
+
+            return View(courses);
         }
 
         public ActionResult About()
@@ -23,6 +40,12 @@ namespace SystemZapisowy.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _unitOfWork.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
