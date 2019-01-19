1338e8ca7e9a1f3099e34604d6c7db544ea9ada9
Szymon Pituła <deesugfx@gmail.com>
2019-01-19 15:54:10 +0100

    Added CourseViewModel, empty CoursesController + default view

---
+++ SystemZapisowy/Controllers/CoursesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SystemZapisowy.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
---
+++ SystemZapisowy/ViewModels/CourseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SystemZapisowy.Models;
+
+namespace SystemZapisowy.ViewModels
+{
+    public class CourseViewModel
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CourseViewModel()
+        {
+            this.Groups = new HashSet<Group>();
+        }
+
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public int SemesterId { get; set; }
+        public int FieldOfStudyId { get; set; }
+
+        public virtual FieldsOfStudy FieldsOfStudy { get; set; }
+        public virtual Semester Semester { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Group> Groups { get; set; }
+    }
+}
